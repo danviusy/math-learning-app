0b005a52e14eb350c1d9511fd4a666c35a267f56
danviusy <68064479+danviusy@users.noreply.github.com>
2025-09-26 01:14:05 +0200

    La til navigation button i Select-Option screen for å komme seg til hjem-skjermen

--- app/src/main/java/com/example/kalkulatorfinal/ui/screens/SelectOptionScreen.kt
+++ app/src/main/java/com/example/kalkulatorfinal/ui/screens/SelectOptionScreen.kt
@@ -8,11 +8,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -120,6 +128,20 @@ fun SelectOptionScreen(navController: NavController, viewModel: CalculatorViewMo
                         }
                     }
                 }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+                shape = RoundedCornerShape(16.dp),
+                contentPadding = PaddingValues(16.dp),
+                onClick = {navController.navigate("start-screen")} )
+            {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Tilbake",
+                    tint = Color.White,
+                    modifier = Modifier.size(32.dp)
+                )
             }
         }
     }
